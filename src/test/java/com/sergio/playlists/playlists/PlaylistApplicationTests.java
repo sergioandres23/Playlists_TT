2eb41a241fb0a11775de85195733a50dd3534664
Sergio Gomez <sergio.gomezv@udea.edu.co>
2025-08-19 15:44:09 -0500

    init project

---
+++ src/test/java/com/sergio/playlists/playlists/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.playlists.playlists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
